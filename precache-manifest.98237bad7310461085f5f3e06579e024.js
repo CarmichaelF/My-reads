48cefe0a4206b5c756228e349e947a236c90c0bc
Carmichael Ferreira Pereira <carmichaelferreira06@gmail.com>
2019-03-17 14:22:35 -0300

    Updates

---
+++ precache-manifest.98237bad7310461085f5f3e06579e024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cecba5f5dbfa6716608a",
+    "url": "/My-reads/static/js/runtime~main.cecba5f5.js"
+  },
+  {
+    "revision": "82f5fe41ea2b71a6b2c1",
+    "url": "/My-reads/static/js/main.82f5fe41.chunk.js"
+  },
+  {
+    "revision": "2cd5570e7af10424559c",
+    "url": "/My-reads/static/js/1.2cd5570e.chunk.js"
+  },
+  {
+    "revision": "82f5fe41ea2b71a6b2c1",
+    "url": "/My-reads/static/css/main.14e30680.chunk.css"
+  },
+  {
+    "revision": "05d495a40348c5f889849b7d67e8e65f",
+    "url": "/My-reads/index.html"
+  }
+];
